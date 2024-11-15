16e3eb50465d8155f7e49345ff0c988d104f4073
metrike <kevinmetri.pro@gmail>
2024-11-15 14:17:16 +0100

    commit

--- optimisation/src/main/java/fr/projet/duo/optimisation/Mapper/ParticipationRequestMapper.java
+++ optimisation/src/main/java/fr/projet/duo/optimisation/Mapper/ParticipationRequestMapper.java
@@ -4,9 +4,14 @@ import fr.projet.duo.optimisation.DTO.ParticipationRequestDTO;
 import fr.projet.duo.optimisation.Entity.ParticipationRequest;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface ParticipationRequestMapper {
 
     ParticipationRequest toEntity(ParticipationRequest participationRequest);
     ParticipationRequestDTO toDTO(ParticipationRequestDTO participationRequestDTO);
+
+    List<ParticipationRequestDTO> toDTOs(List<ParticipationRequest> participationRequests);
+    List<ParticipationRequest> toEntities(List<ParticipationRequestDTO> participationRequestDTOs);
 }
